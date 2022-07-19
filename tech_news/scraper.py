d4a20d3a4706b50fc630b35c9fc7d40591b14411
Diego Silva <coxcorp@gmail.com>
2022-07-19 19:40:33 -0300

    requisito 04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -30,9 +30,32 @@ def scrape_next_page_link(html_content):
     return next
 
 
+print(scrape_novidades(fetch("https://blog.betrybe.com/")))
+
+
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    url = selector.css('link[rel="canonical"]::attr(href)').get()
+    title = selector.css("h1.entry-title::text").get()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author > a::text").get()
+    comments_count = len(selector.css("ol.comment-list li").getall())
+    summary = "".join(
+        selector.css("div.entry-content p:nth-child(2) *::text").getall()
+    )
+    tags = selector.css('a[rel="tag"]::text').getall()
+    category = selector.css("a > span.label::text").get()
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count,
+        "summary": summary,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
